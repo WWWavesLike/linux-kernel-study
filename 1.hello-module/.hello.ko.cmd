2e44512f82e718f2a62b108287ee80249b9eb516
WWWavesLike <hc5189@naver.com>
2025-12-03 21:27:59 +0900

    first commit

---
+++ 1.hello-module/.hello.ko.cmd
@@ -0,0 +1,1 @@
+savedcmd_hello.ko := ld -r -m elf_x86_64 -z noexecstack --no-warn-rwx-segments --build-id=sha1  -T /home/waveslike/vm/linux-6.17.9/scripts/module.lds -o hello.ko hello.o hello.mod.o .module-common.o
